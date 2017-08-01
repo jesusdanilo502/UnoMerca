535ad4e852100ce7c25186d5298f949947ebf2d0
ONEMAS-JEUS DANILO <serviciosonemas@gmail.com>
2017-07-31 21:02:18 -0500

    Configuraciòn Inicial Proyecto

---
+++ config/global.php
@@ -0,0 +1,18 @@
+<?php
+  //Ip de la pc del servidor de base de datos
+  define("DB_HOST","localhost");
+
+  // NOMBRE DE LA BASE DE DATOS
+  define("DB_NAME","dbsistema");
+
+  //USUARIO DE LA BASE DE DATOS
+  define("DB_USERNAME","root");
+
+  //CONTRASEÑA DE LA BASE DE DATOS
+  define("DB_PASSWORD","");
+  // CODIFICACION DE CARACTERES
+  define("DB_ENCODE","utf8");
+
+  //DEFINIMOS UNA CONSTANTE COMO NOMBRE DEL PROYECTO
+  define("PRO_NOMBRE","ITVentas");
+ ?>
