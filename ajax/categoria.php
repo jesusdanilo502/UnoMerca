3a4a1f5ca8703d0dfcf6f717bef2a09d3d168906
ONEMAS-JEUS DANILO <serviciosonemas@gmail.com>
2017-08-02 21:48:15 -0500

    _post

--- ajax/categoria.php
+++ ajax/categoria.php
@@ -3,4 +3,6 @@
   $categoriaone= new Categoria();
 
   $idcategoria=isset($_POST["idcategoria"])? limpiarCadena($_POST["idcategoria"]):"";
+  $nombre=isset($_POST["nombre"])? limpiarCadena($_POST["nombre"]):"";
+  $descripcion=isset($_POST["descripcion"])? limpiarCadena($_POST["descripcion"]):"";
  ?>
