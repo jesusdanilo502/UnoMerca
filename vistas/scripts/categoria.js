23d3591fb36db77f6e2449d253ac449c2a071f6e
onemas <jesusdanilo502@users.noreply.github.com>
2018-02-27 21:22:00 -0500

    se crea form para permitir la inserción de categorias

--- vistas/scripts/categoria.js
+++ vistas/scripts/categoria.js
@@ -4,6 +4,13 @@
   function init(){
     mostrarform(false);
     listar();
+
+    //ejecuto boton guardar para cuando de click me llame la función guardaryditar del formulario
+      //y se ejecute el proceso
+      $("#formulario").on("submit",function (e)
+      {
+          guardaryeditar(e);
+      })
   }
 
   // funciòn limpiar
@@ -64,5 +71,28 @@
     "iDisplayLength": 5, // paginación
     "order":[[0, "desc"]] // Ordenar (Columna,orden)
   }).DataTable();
+  }
+  // Función para guardar y editar
+  function guardaryeditar(e)
+  {
+   e.preventDefault(); // No se activara la acción predeterminada del evento segun la librearia ajax
+      $("#btnGuardar").prop("disabled",true);
+      var formData= new FormData($("#formulario")[0]);
+
+      $.ajax({
+          url: "../ajax/categoria.php?op=guardaryeditar",
+          type: "POST",
+          data: formData,
+          contentType: false,
+          processData: False,
+
+          success: function (datos)
+          {
+           alert(datos);
+           mostrarform(false);
+           tabla.ajax.reload();
+          }
+      });
+      limpiar();
   }
   init();
